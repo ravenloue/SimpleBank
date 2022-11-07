3f4e231a68a48804a75bcd28c70b25168c8d1f6f
ravenloue <42080046+ravenloue@users.noreply.github.com>
2022-11-06 18:32:44 -0600

    Added an icon and screenshot
    Polishing up the little things before loading the logic for the banking part

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 
 
@@ -16,6 +17,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Simple Bank");
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/imgs/bankicon.png")));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
